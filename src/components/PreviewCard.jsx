5c1829d86c742fb33fc924e106039aebcdb09d6a
Luke H. Walton <lukehwalton@icloud.com>
2023-07-12 09:43:46 -0700

    committing for merge with fetch function main

--- src/components/PreviewCard.jsx
+++ src/components/PreviewCard.jsx
@@ -3,18 +3,17 @@ import {Card, Button} from 'react-bootstrap'
 const PreviewCard = ( { movie } ) => {
   return (
     <Card style={{
-      width: '100px',
-      margin: '10px',
+      width: '150px',
+      margin: '5px',
       textAlign: 'center'
     }}>
       <Card.Img
         variant='top'
         src={`https://image.tmdb.org/t/p/w500/${movie.poster_path}`}
-        width='100px'
         ></Card.Img>
       <Card.Body>
-        <Card.Title>{movie.title}</Card.Title>
-        <Button>View details</Button>
+        <p>{movie.title}</p>
+        <Button variant='dark'>View details</Button>
       </Card.Body>
     </Card>
   )
