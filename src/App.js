112c9c8ff655e6a3fa1825866f0c87aa479d6f5b
F3riko <vyrydy@mail.ru>
2023-07-12 17:10:32 +0300

    App.js and neccessary routing for both public and protected routes, basic components for testing

--- src/App.js
+++ src/App.js
@@ -1,24 +1,74 @@
-import logo from './logo.svg';
-import './App.css';
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Outlet,
+  Navigate,
+  useParams,
+} from "react-router-dom";
+import "./App.css";
+
+// The following components are placeholder for testing and demo purposes,
+// when the specified components are ready the placeholder should have been replaced
+// wian an actual ones
+
+const Layout = () => {
+  return (
+    <>
+      <nav>
+        <h1>Navigation component placeholder</h1>
+      </nav>
+      <Outlet />
+    </>
+  );
+};
+
+const MainPage = () => {
+  return <h1>Main page</h1>;
+};
+
+const MoviePage = () => {
+  const { id } = useParams();
+  return <h1>Movie with id {id}</h1>;
+};
+
+const AuthRequired = () => {
+  // AuthFunc(id) goes in this layout component
+  const isLoggedIn = true;
+  if (!isLoggedIn) {
+    return <Navigate to="/" />;
+  }
+  return <Outlet />;
+};
+
+const UserPage = () => {
+  const { id } = useParams();
+
+  return <h1>This page is protected by the Auth func. The user id is {id}</h1>;
+};
+
+const SearchResults = () => {
+  // useSearchParams hook goes here
+  return <h1>Search Results</h1>;
+};
+
+// End of testing section
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<MainPage />} />
+          <Route path="movie/:id" element={<MoviePage />} />
+          <Route element={<AuthRequired />}>
+            <Route path="user/:id" element={<UserPage />} />
+          </Route>
+          <Route path="searchResults" element={<SearchResults />} />
+          <Route path="*" element={<h1>404 - Page not found goes here</h1>} />
+        </Route>
+      </Routes>
+    </Router>
   );
 }
 
