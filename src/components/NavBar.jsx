cf4244bce411db53e6a2b742a7b3c33a83259c18
Luke H. Walton <lukehwalton@icloud.com>
2023-07-13 04:28:18 -0700

    committing navbar before pulling main

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -7,6 +7,9 @@ import Button from 'react-bootstrap/Button'
 import '../bootstrap.min.css'
 
 const NavComponent = ({ isLoggedIn, onSearch, onLogin, onSignup } ) => {
+  const handleClick = () => {
+    console.log('search button pushed')
+  }
 
   return(
     <Container>
@@ -14,11 +17,19 @@ const NavComponent = ({ isLoggedIn, onSearch, onLogin, onSignup } ) => {
         <Col>
         <Form controlID='search'>
           <Form.Group>
-            <Form.Control type='search'></Form.Control>
+            <Form.Control type='search' placeholder='Search...'></Form.Control>
           </Form.Group>
         </Form>
+        </Col>
+        <Col>
+          <Button variant='primary' onClick={handleClick}>Search</Button>
+        </Col>
+        <Col>
+
         </Col>
       </Row>
     </Container>
   )
 }
+
+export default NavComponent;
