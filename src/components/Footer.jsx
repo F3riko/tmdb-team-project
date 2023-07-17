49ef299787d7f73c9addeabff0852a572ece40d5
Luke H. Walton <lukehwalton@icloud.com>
2023-07-17 10:45:41 -0700

    committing in anticipation of pulling main

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -6,8 +6,9 @@ const Footer = () => {
   return (
     <Stack direction='horizontal'>
       <small>&copy; 2023 PAL Industries</small>
-      <img src={tmdb} width='150px' id='tmdb-logo'/>
+      <img src={tmdb} width='150px' id='tmdb-logo' alt='The Movie Database logo'/>
       <small id='tmdb-disclosure'>This product uses the TMDB API but is not endorsed or certified by TMDB.</small>
+      <div id='responsive-voice-license'><a href="https://responsivevoice.org">ResponsiveVoice-NonCommercial</a> licensed under <a href="https://creativecommons.org/licenses/by-nc-nd/4.0/"><img title="ResponsiveVoice Text To Speech" src="https://responsivevoice.org/wp-content/uploads/2014/08/95x15.png" alt="95x15" width="95" height="15" /></a></div>
     </Stack>
   )
 }
--- src/components/MovieGallery.jsx
+++ src/components/MovieGallery.jsx
@@ -4,24 +4,21 @@ import PreviewCard from "./PreviewCard";
 import Row from 'react-bootstrap/Row'
 import Col from 'react-bootstrap/Col'
 import '../bootstrap.min.css'
+import './MovieGallery.css'
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faSearch } from "@fortawesome/free-solid-svg-icons";
 
 const movieGallery = ({ moviesList, listType }) => {
   return(
-    <>
-      <Container style={{
-        border: '1px solid lightGray'
-      }}>
-        <h3>{`${listType} Movies`}</h3>
-        <Row>
-          {moviesList.slice(0,4).map(movie => 
-          <Col className='preview'>
-            <PreviewCard key={movie.id} movie={movie} />
-          </Col>)}
-        </Row>
-      </Container>
-    </>
+    <Container className='gallery-frame'>
+      <h3>{`${listType} Movies`}</h3>
+      <Row>
+        {moviesList.slice(0,4).map(movie => 
+        <Col className='preview'>
+          <PreviewCard key={movie.id} movie={movie} />
+        </Col>)}
+      </Row>
+    </Container>
   )
 }
 
